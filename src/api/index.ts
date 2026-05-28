7d5ac865d84d55d0c04fa758526dad4d90b6995d
Thami Bittencourt <thamiris.bittencourt@gmail.com>
2026-05-28 21:02:52 +0200

    fix: index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,23 +1,31 @@
-export {
-  api,
-  petsApi,
-  vetsApi,
-  appointmentsApi,
-  medicationsApi,
-  symptomsApi,
-  caresApi,
-  vaccinesApi,
-  BASE_URL,
-} from './client'
+// src/api/index.ts
+export { api, BASE_URL, getToken, setToken, clearToken } from './client'
+export type { ApiResponse, ApiError } from './client'
+
+export { authApi }                from './auth'
+export { petsApi }                from './pets'
+export { vetsApi }                from './vets'
+export { appointmentsApi }        from './appointments'
+export { medicationsApi }         from './medications'
+export { symptomsApi }            from './symptoms'
+export { caresApi }               from './cares'
+export { notesApi }               from './notes'
+export { medicalProfilesApi }     from './medicalProfiles'
+export { usersApi }               from './users'
+export { default as vaccinesApi } from './vaccines'
 
 export type {
-  ApiResponse,
-  ApiError,
-  ApiPet,
-  ApiVet,
-  ApiAppointment,
-  ApiMedication,
-  ApiSymptom,
-  ApiCare,
-  ApiVaccine,
-} from './client'
+  ApiPet, ApiVet, ApiAppointment, ApiMedication,
+  ApiSymptom, ApiCare, ApiVaccine, ApiNote,
+  ApiMedicalProfile, ApiUser,
+  CreatePetDto, UpdatePetDto,
+  CreateVetDto, UpdateVetDto,
+  CreateVaccineDto, UpdateVaccineDto,
+  CreateMedicationDto, UpdateMedicationDto,
+  CreateSymptomDto, UpdateSymptomDto,
+  CreateCareDto, UpdateCareDto,
+  CreateNoteDto, UpdateNoteDto,
+  CreateAppointmentDto, UpdateAppointmentDto,
+  UpsertMedicalProfileDto,
+  LoginDto, RegisterDto, AuthResponse, AuthUser,
+} from './types'
