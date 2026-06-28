0a6e1e5abf4d3afcee7510cec64f46d069722fa0
Thami Bittencourt <thamiris.bittencourt@gmail.com>
2026-06-28 15:11:52 +0200

    fix: import VaccinesContext

--- src/context/VaccinesContext.tsx
+++ src/context/VaccinesContext.tsx
@@ -1,4 +1,4 @@
-import createContext, { useContext, useState, useCallback, useEffect, type ReactNode } from "react";
+import { createContext, useContext, useState, useCallback, useEffect, type ReactNode } from "react";
 import { usePetsContext } from "./PetsContext";
 import { getVaccStatus } from "../utils/vaccUtils";
 import type { VaccineRecord } from "../utils/vaccUtils";
